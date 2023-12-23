3e7e75e63b6d659d0bd9f575fdac439c334c6fa8
Valentin Levin <levin_valentin@mail.ru>
2023-12-23 17:14:04 +0600

    solution #1

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,32 +1,70 @@
 package com.epam.mjc;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class InterfaceCreator {
 
     public Predicate<List<String>> isValuesStartWithUpperCase() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return values -> {
+            for (String value : values) {
+                if (value == null || value.isEmpty() || value.charAt(0) < 'A' || value.charAt(0) > 'Z')
+                    return false;
+            }
+
+            return true;
+        };
     }
 
     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return values -> {
+            int valueCount = values.size();
+            for (int i = 0; i < valueCount; i++) {
+                int value = values.get(i);
+                if (value % 2 == 0) {
+                    values.add(value);
+                }
+            }
+        };
     }
 
     public Supplier<List<String>> filterCollection(List<String> values) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return () -> {
+            Predicate<String> isStartsWithUpperCase = value -> value != null && !value.isEmpty() && value.charAt(0) >= 'A' && value.charAt(0) <= 'Z';
+            Predicate<String> isLineEndsWithDot = value -> value != null && !value.isEmpty() && value.charAt(value.length() - 1) == '.';
+            Predicate<String> isLineHasAtLeast3Words = value -> value != null && !value.isEmpty() && value.split("\\W").length > 3;
+
+            return values.stream()
+                    .filter(
+                            isStartsWithUpperCase
+                                    .and(isLineEndsWithDot)
+                                    .and(isLineHasAtLeast3Words)
+                    )
+                    .collect(Collectors.toList());
+        };
     }
 
     public Function<List<String>, Map<String, Integer>> stringSize() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list -> {
+            Map<String, Integer> map = new HashMap<>();
+            for (String value: list) {
+                map.put(value, value.length());
+            }
+            return map;
+        };
     }
 
     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (list1, list2) -> {
+            List<Integer> result = new ArrayList<>();
+            result.addAll(list1);
+            result.addAll(list2);
+            return result;
+        };
     }
 }
